0fd78ec7c93e5c2715683622d20240ac0a18f637
TungDT <tung.daothanhtung@gmail.com>
2019-10-31 11:11:01 +0700

    add log, context_simple

---
+++ context_try/cmdtimeout/cmdtimeout.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	// Pass a context with a timeout to tell a blocking function that it
+	// should abandon its work after the timeout elapses.
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+	//time.Sleep(60 * time.Millisecond)
+	select {
+	case <-time.After(20 * time.Millisecond):
+		fmt.Println("overslept")
+		fmt.Println("ctx.Err() 1:", ctx.Err()) // nil
+	case <-ctx.Done():
+		fmt.Println("ctx.Err() 2:", ctx.Err()) // context deadline exceeded
+	}
+}
--- query_mysql/go.mod
+++
@@ -1,6 +0,0 @@
-module github.com/daominah/20170415_HelloGo/query_mysql
-
-require (
-	github.com/astaxie/beego v1.11.1
-	github.com/go-sql-driver/mysql v1.4.1
-)
