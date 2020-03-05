222307c36d430c4ae153b380092c07e6ed49e33d
TungDT <tung.daothanhtung@gmail.com>
2020-03-05 16:22:41 +0700

    simplify the CPUs calculation duration example

--- channel2/channel2.go
+++ channel2/channel2.go
@@ -5,42 +5,33 @@ import (
 	"time"
 )
 
-// Sum returns b + (b+1) + (b+2) + .. + (e-1)
-func Sum(b int, e int) int {
-	ret := 0
-	for i := b; i < e; i++ {
+// Sum returns n by calculating 1+1+1+..
+func Sum(n int64) int64 {
+	ret := int64(0)
+	for i := int64(0); i < n; i++ {
 		ret += 1
 	}
 	return ret
 }
 
 func main() {
-	n := 30000000000 // 30e9
-	sum := 0
+	n := int64(30000000000) // 30e9
+	sum := int64(0)
 	bt := time.Now()
 
-	// Expectations of (nWorkers,dur): (1, 8s), (2, 4s), (4, 2s), ..
-	// but why (8, 2s) too ???
 	nWorkers := 4
-	retChan := make(chan int)
-	segs := make([]int, nWorkers)
+	sumChan := make(chan int64, nWorkers)
 	for i := 0; i < nWorkers; i++ {
-		segs[i] = (i + 1) * n / nWorkers
-	}
-	for i, e := range segs {
-		b := 0
-		if i > 0 {
-			b = segs[i-1]
-		}
-		e := e
-		go func() { retChan <- Sum(b, e) }()
+		go func() { sumChan <- Sum(n / int64(nWorkers)) }()
 	}
 	for i := 0; i < nWorkers; i++ {
-		sum += <-retChan
+		sum += <-sumChan
 	}
 
-	//
 	dur := time.Now().Sub(bt)
 	fmt.Println("dur:", dur)
 	fmt.Println("sum:", sum)
+
+	// Results on Intel Core i5-8265U (nWorkers,dur):
+	// (1, 8s), (2, 4s), (4, 2s), (8, 2s). Why 8 CPUs still need 2s?
 }
