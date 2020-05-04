de341e995a10d640a780e68b28312b067d6b8e09
Đào Thanh Tùng <tung.dao@techx.vn>
2020-05-04 09:06:58 +0700

    fix xml unparse

--- encoding_try/xml.go
+++ encoding_try/xml.go
@@ -12,27 +12,28 @@ type Lala struct {
 }
 
 func main() {
-	lan := Lala{CuoiXinh: true, NgoNgo: "yo", Emo: []int{2, 3, 4}}
+	lan := Lala{CuoiXinh: true, NgoNgo: "y", Emo: []int{2, 3, 4}}
 	temp, err := xml.MarshalIndent(lan, "", "    ")
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("\n%v", string(temp))
+	log.Printf("temp:\n%s", temp)
 
-	s := `<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-			<soap:Body>
-				<soap:Fault>
-					<faultcode>soap:Server</faultcode>
-					<faultstring>Fault occurred while processing.</faultstring>
-						<detail>
-							<ns1:ExceptionName xmlns:ns1="http://ws.token.authentication.fo.fss.com/"/>
-						</detail>
-				</soap:Fault>
-			</soap:Body>
-		</soap:Envelope>`
-	_ = s
-	var v interface{}
+	//s := `<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+	//		<soap:Body>
+	//			<soap:Fault>
+	//				<faultcode>soap:Server</faultcode>
+	//				<faultstring>Fault occurred while processing.</faultstring>
+	//					<detail>
+	//						<ns1:ExceptionName xmlns:ns1="http://ws.token.authentication.fo.fss.com/"/>
+	//					</detail>
+	//			</soap:Fault>
+	//		</soap:Body>
+	//	</soap:Envelope>`
+	//_ = s
+
+	var v Lala
 	err = xml.Unmarshal(temp, &v)
 	if err != nil {
 		log.Fatal("cannot unmarshal", err)
