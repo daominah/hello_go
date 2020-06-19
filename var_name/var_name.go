9bab26c5198bf822e1b725e3755acd14b109319f
Đào Thanh Tùng <tung.dao@techx.vn>
2020-06-19 16:28:30 +0700

    add var_name

---
+++ var_name/var_name.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	α, β, γ := 2, 3, 4
+	log.Println(α, β, γ)
+	Δ, δ, ε, π, Ω, ω := "Delta", "delta", "epsilon", "pi", "omega", "Omega"
+	log.Println(Δ, δ, ε, π, Ω, ω)
+	ಠ_ಠ := "hmm.."
+	log.Printf("ಠ_ಠ: %v\n", ಠ_ಠ)
+}
