2c475b13c7a9cd1bad7aade3f3ed891820ce4bd1
Đào Thanh Tùng <tung.dao@techx.vn>
2020-08-19 11:19:48 +0700

    add etcd_try

---
+++ etcd_try/etcd_get.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/etcdserver/api/v3rpc/rpctypes"
+)
+
+func main() {
+	etcdCli, err := clientv3.New(clientv3.Config{
+		Endpoints: []string{
+			"192.168.99.100:2379",
+			"192.168.99.101:2379",
+			"192.168.99.102:2379",
+		},
+		DialTimeout: 30 * time.Second,
+		Username:    "root",
+		Password:    "123qwe",
+	})
+	if err != nil {
+		log.Fatalf("toang: %v", err)
+	}
+	defer etcdCli.Close()
+
+	ctx, cxl := context.WithTimeout(context.Background(), 5*time.Second)
+	ret, err := etcdCli.Get(ctx, "key", clientv3.WithPrefix())
+	cxl()
+	if err != nil {
+		switch err {
+		case context.Canceled:
+			log.Printf("canceled: %v", err)
+		case context.DeadlineExceeded:
+			log.Printf("timeout: %v", err)
+		case rpctypes.ErrEmptyKey:
+			log.Printf("client error: %v", err)
+		default:
+			log.Printf("bad etcd servers: %v", err)
+		}
+	}
+	log.Printf("ret: %#v", ret)
+	for _, kv := range ret.Kvs {
+		log.Printf("k: %s, v: %s", kv.Key, kv.Value)
+	}
+}
---
+++ etcd_try/etcd_lock.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/clientv3/concurrency"
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile | log.Lmicroseconds)
+
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints: []string{
+			"192.168.99.100:2379",
+			"192.168.99.101:2379",
+			"192.168.99.102:2379",
+		},
+		DialTimeout: 30 * time.Second,
+		Username:    "root",
+		Password:    "123qwe",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cli.Close()
+	log.Printf("connected to etcd cluseter: %v", cli.Endpoints())
+
+	// all locks created by this session have a TTL of 3s
+	// (actual TTL value is at least the TTL)
+	s1, err := concurrency.NewSession(cli, concurrency.WithTTL(5))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer s1.Close()
+
+	const lockName = "/myLock2"
+	m1 := concurrency.NewMutex(s1, lockName)
+
+	s2, err := concurrency.NewSession(cli, concurrency.WithTTL(10))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer s2.Close()
+	m2 := concurrency.NewMutex(s2, lockName)
+
+	log.Printf("s1 try to acquire the lock")
+	if err := m1.Lock(context.TODO()); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("acquired lock for s1")
+
+	go func() {
+		log.Printf("s2 try to acquire the lock")
+		if err := m2.Lock(context.TODO()); err != nil {
+			log.Fatal(err)
+		}
+		log.Println("acquired lock for s2")
+	}()
+
+	time.Sleep(1 * time.Second)
+
+	// skip this if to simulate crash after lock
+	if !true {
+		log.Printf("s1 try to unlock")
+		if err := m1.Unlock(context.TODO()); err != nil {
+			log.Fatal(err)
+		}
+		log.Println("released lock for s1")
+	}
+
+	time.Sleep(100 * time.Second)
+}
