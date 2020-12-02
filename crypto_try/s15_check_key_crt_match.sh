be6c56ab73f44ab87425210b10f4cddb28d2a5d8
Đào Thanh Tùng <tung.dao@techx.vn>
2020-12-02 10:17:50 +0700

    more option check key pair 2

--- crypto_try/s15_check_key_crt_match.sh
+++ crypto_try/s15_check_key_crt_match.sh
@@ -3,8 +3,8 @@
 export privatePath=myorg0.key
 export publicPath=myorg0.crt
 
-openssl rsa --in --noout --modulus ${privatePath}
+openssl rsa --noout --modulus --in ${privatePath}
 
 # one of 2 following command is good, depend on cert format
-openssl x509 --inform pem --in --noout --modulus ${publicPath}
-openssl x509 --inform der --in --noout --modulus ${publicPath}
+openssl x509 --inform pem --noout --modulus --in ${publicPath}
+openssl x509 --inform der --noout --modulus --in ${publicPath}
