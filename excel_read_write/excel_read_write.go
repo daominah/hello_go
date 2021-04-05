7a2149c2073ffe6b7ca5ef42ec0ccdf6311326fd
Đào Thanh Tùng <tung.dao@techx.vn>
2021-04-05 17:10:52 +0700

    update tealeg/xlsx to v3

--- excel_read_write/excel_read_write.go
+++ excel_read_write/excel_read_write.go
@@ -1,97 +1,38 @@
 package main
 
 import (
-	"encoding/csv"
-	"fmt"
-	"os"
-	"strings"
+	"log"
 
-	"sort"
-
-	"github.com/mywrap/log"
-	"github.com/mywrap/textproc"
-	"github.com/tealeg/xlsx"
+	xlsx "github.com/tealeg/xlsx/v3"
 )
 
-func XPrintRow(row *xlsx.Row) {
-	cellStrs := []string{}
-	for _, cell := range row.Cells {
-		cellS := cell.String()
-		cellS = fmt.Sprintf("%12v", cellS)
-		cellStrs = append(cellStrs, cellS)
-	}
-	result := strings.Join(cellStrs, " ")
-	fmt.Println(result)
-}
-
 func main() {
-	xlFile, err := xlsx.OpenFile("/home/tungdt/Desktop/client_name_sub.xlsx")
+	// read
+
+	xlFile, err := xlsx.OpenFile("people_read.xlsx")
 	if err != nil {
 		log.Fatal(err)
 	}
 	if len(xlFile.Sheets) == 0 {
-		log.Fatal("no sheets")
+		log.Fatal("error the file has no sheets")
 	}
-
-	accs := make(map[string]string)
-	subAccs := make(map[string]string)
-
-	sheet0 := xlFile.Sheets[0]
-	for i, row := range sheet0.Rows {
-		if i == 0 {
-			fmt.Println("Fields: ")
-			XPrintRow(row)
-		} else {
-			if len(row.Cells) != 3 {
-				continue
-			}
-			acc, name, subAcc := row.Cells[0].String(), row.Cells[1].String(), row.Cells[2].String()
-			acc, name, subAcc = strings.TrimSpace(acc), strings.TrimSpace(name), strings.TrimSpace(subAcc)
-			name = textproc.RemoveVietnamDiacritic(name)
-			name = strings.ToUpper(name)
-			name = strings.ReplaceAll(name, `"`, ``)
-			accs[acc] = name
-			subAccs[subAcc] = name
+	type Person struct {
+		Name      string  `xlsx:"0"`
+		WeightKgs float64 `xlsx:"1"`
+		IsMale    bool    `xlsx:"2"`
+	}
+	read := make([]Person, 0)
+	xlFile.Sheets[0].ForEachRow(func(row *xlsx.Row) error {
+		if row.GetCoordinate() == 0 {
+			return nil
 		}
-
-		//if i == 10 {
-		//	break
-		//}
-	}
-
-	rows := make([][]string, 0)
-	for k, v := range accs {
-		rows = append(rows, []string{k, v})
-	}
-	for k, v := range subAccs {
-		rows = append(rows, []string{k, v})
-	}
-	sort.Sort(SortByAcc(rows))
-	log.Debugf("%#v", rows)
-
-	f, _ := os.OpenFile("/home/tungdt/Desktop/names.csv",
-		os.O_TRUNC|os.O_CREATE|os.O_RDWR,
-		0644)
-	w := csv.NewWriter(f)
-	for _, record := range rows {
-		if err := w.Write(record); err != nil {
-			log.Fatal("error writing record to csv:", err)
+		var person Person
+		err := row.ReadStruct(&person)
+		if err != nil {
+			log.Printf("error read row %v: %v", row.GetCoordinate(), err)
 		}
-	}
-	w.Flush()
-	if err := w.Error(); err != nil {
-		log.Fatalf("error Flush: %v", err)
-	}
-	log.Printf("ok")
-}
-
-type SortByAcc [][]string
-
-func (a SortByAcc) Len() int      { return len(a) }
-func (a SortByAcc) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a SortByAcc) Less(i, j int) bool {
-	if len(a[i]) < 1 || len(a[j]) < 1 {
-		return false
-	}
-	return a[i][0] < a[j][0]
+		read = append(read, person)
+		return nil
+	})
+	log.Printf("read: %#v", read)
 }
--- excel_read_write/go.mod
+++ excel_read_write/go.mod
@@ -2,4 +2,4 @@ module github.com/daominah/hello_go/excel_read_write
 
 go 1.12
 
-require github.com/tealeg/xlsx v1.0.3
+require github.com/tealeg/xlsx/v3 v3.2.3
