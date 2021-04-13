d2efeace89d388730e3350e18f3a35462a0ac015
Đào Thanh Tùng <tung.dao@techx.vn>
2021-04-13 15:26:44 +0700

    better example utf16

--- encodings/utf16.go
+++ encodings/utf16.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
 	"golang.org/x/text/encoding/unicode"
 )
@@ -9,11 +9,15 @@ import (
 func main() {
 	s := "exampleString0"
 	encoder16be := unicode.UTF16(unicode.BigEndian, unicode.IgnoreBOM).NewEncoder()
-	encoded, err := encoder16be.Bytes([]byte(s))
-	fmt.Printf("encoder16be Bytes: %v, %#v\n", err, encoded)
+	encoded16, err := encoder16be.Bytes([]byte(s))
+	log.Printf("encoder16be Bytes: %v, %#v\n", err, encoded16)
+
+	decoder16be := unicode.UTF16(unicode.BigEndian, unicode.IgnoreBOM).NewDecoder()
+	decoded, err := decoder16be.Bytes(encoded16)
+	log.Printf("decoder16be Bytes: %v\n", string(decoded))
 
 	encoder8 := unicode.UTF8.NewEncoder()
-	encoded, err = encoder8.Bytes([]byte(s))
-	fmt.Printf("encoder8 Bytes: %v, %#v\n", err, encoded)
-	fmt.Printf("slice bytes: %#v\n", []byte(s))
+	encoded8, err := encoder8.Bytes([]byte(s))
+	log.Printf("encoder8 Bytes: %v, %#v\n", err, encoded8)
+	log.Printf("slice bytes: %#v\n", []byte(s))
 }
