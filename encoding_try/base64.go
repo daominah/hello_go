25dd046ccd712142bfdbed5b6b12194cb6152c72
Đào Thanh Tùng <tung.dao@difisoft.com>
2021-10-01 09:40:32 +0700

    typo

--- encoding_try/base64.go
+++ encoding_try/base64.go
@@ -8,22 +8,25 @@ import (
 func main() {
 	s := `username:password`
 	base64ed := base64.StdEncoding.EncodeToString([]byte(s))
-	log.Println(base64ed)
+	println("base64ed:")
+	println(base64ed)
+	println("-----------------------------------------------------------")
 
-	if false {
+	if false { // decode method 0
 		decodedB, err := base64.StdEncoding.DecodeString(base64ed)
 		if err != nil {
 			log.Fatal(err)
 		}
 		decoded := string(decodedB)
-		log.Println(decoded)
-	} else {
+		println(decoded)
+	} else { // decode method 1
 		decodedB := make([]byte, base64.StdEncoding.DecodedLen(len(base64ed)))
 		n, err := base64.StdEncoding.Decode(decodedB, []byte(base64ed))
 		if err != nil {
 			log.Fatal(err)
 		}
 		decoded := string(decodedB[:n])
-		log.Println(decoded)
+		println("decoded:")
+		println(decoded)
 	}
 }
