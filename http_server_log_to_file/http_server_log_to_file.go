d6b970776c070fd8de3b5fa3eceab9394a65428e
DaoThanhTung <tung.daothanhtung@gmail.com>
2025-03-26 10:35:44 +0700

    add log line number, time zone to vn

--- http_server_log_to_file/http_server_log_to_file.go
+++ http_server_log_to_file/http_server_log_to_file.go
@@ -3,54 +3,104 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
 
 var (
-	logFile *os.File
-	mu      sync.Mutex
+	logFile    *os.File
+	lineNumber int
+	mu         sync.Mutex
 )
 
 func main() {
-	var err error
-	logFile, err = os.OpenFile("server.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+	const logFileName = "server.log"
+
+	// just open, count the number of lines, then close
+	tmpF, err := os.Open(logFileName)
+	if err == nil { // if the file exists
+		scanner := bufio.NewScanner(tmpF)
+		for scanner.Scan() {
+			lineNumber++
+		}
+		tmpF.Close()
+	}
+
+	// reopen the file as a global var, only close when the server is stopped
+	logFile, err = os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
 	if err != nil {
-		fmt.Println("Error opening log file:", err)
-		return
+		log.Fatalf("error open log file: %v", err)
 	}
 	defer logFile.Close()
 
+	tmp, err := time.Parse(time.RFC3339, "2006-01-02T15:04:05+07:00")
+	if err != nil {
+		log.Fatalf("error init VN time zone: %v", err)
+	}
+	vnTimeZone := tmp.Location()
+
 	handler := http.NewServeMux()
+	allowedOrigins := []string{
+		"http://localhost", // dangerous, only for testing
+		"https://yugiodd.github.io",
+	}
 
 	// example request:
-	// http://127.0.0.1:20991/log?odds=0.2333&nStarters=3&deckSize=40&handSize=5&min=1&max=3
+	// http://localhost:20991/log?odds=0.2333&nStarters=3&deckSize=40&handSize=5&min=1&max=3
 	handler.HandleFunc("/log", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			return
 		}
 
-		logEntry := fmt.Sprintf("%-30v %-30v %v\n",
-			time.Now().UTC().Format(time.RFC3339),
-			r.RemoteAddr,
-			r.URL.RawQuery)
+		origin := r.Header.Get("Origin")
+
+		isAllowed := origin == ""
+		for _, allowedOrigin := range allowedOrigins {
+			if strings.Contains(origin, allowedOrigin) {
+				isAllowed = true
+				break
+			}
+		}
+		if !isAllowed {
+			http.Error(w, "Origin not allowed", http.StatusForbidden)
+			return
+		}
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+
+		remoteAddr := r.RemoteAddr
+		// handle if the server is behind cloudflare tunnel or a proxy
+		if forwardedFor := r.Header.Get("X-Forwarded-For"); forwardedFor != "" {
+			remoteAddr = forwardedFor
+		} else if cfConnectingIP := r.Header.Get("CF-Connecting-IP"); cfConnectingIP != "" {
+			remoteAddr = cfConnectingIP
+		}
 
 		mu.Lock()
-		defer mu.Unlock()
+		lineNumber++
+		logEntry := fmt.Sprintf("%-7v%-25v %-43v %v\n",
+			lineNumber,
+			time.Now().In(vnTimeZone).Format(time.RFC3339),
+			remoteAddr,
+			r.URL.RawQuery)
 		if _, err := logFile.WriteString(logEntry); err != nil {
+			mu.Unlock()
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 			return
 		}
+		mu.Unlock()
 
 		w.WriteHeader(http.StatusOK)
 	})
 
 	// example request:
-	// http://127.0.0.1:20991/view?limit=100  // limit count from the end of the file
+	// http://localhost:20991/view?limit=50  // limit count from the end of the file
 	handler.HandleFunc("/view", func(w http.ResponseWriter, r *http.Request) {
 		limit := 100
 		if l := r.URL.Query().Get("limit"); l != "" {
